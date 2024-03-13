f41af4f43a648206ba303c5cf20255afd55c7ab2
Alexey Mikhnenko <AlexeyMikhnenko@yandex.ru>
2024-03-13 12:22:51 +0300

    Skip button bug fix

--- static/scripts/graph_editor/edge.js
+++ static/scripts/graph_editor/edge.js
@@ -172,7 +172,7 @@ export class Edge {
         const circle1 = this.node1.getCircle();
         const circle2 = this.node2.getCircle();
 
-        const MAX_HEIGHT_CHANGE = (force ? 0.1 : height + Math.abs(this.#currentHeight) + 1e-9);
+        const MAX_HEIGHT_CHANGE = (!force ? 0.1 : height + Math.abs(this.#currentHeight) + 1e-9);
         if (this.#currentHeight < height) {
             this.#currentHeight += Math.min(height - this.#currentHeight, MAX_HEIGHT_CHANGE);
         } else {
