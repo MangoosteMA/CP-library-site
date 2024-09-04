6d6c7d191e6e0fa70e886c4ccf2929228d261613
Alexey Mikhnenko <AlexeyMikhnenko@yandex.ru>
2024-09-05 00:40:37 +0300

    Moved app to the global scope.

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask   import Flask
 from secrets import token_hex
 from views   import view
 
+app = Flask(__name__)
+app.secret_key = token_hex()
+app.register_blueprint(view, url_prefix='/')
+
 def main():
-    app = Flask(__name__)
-    app.secret_key = token_hex()
-    app.register_blueprint(view, url_prefix='/')
     app.run(debug=True, port=1337)
 
 if __name__ == '__main__':
