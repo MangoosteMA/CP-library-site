ba5d10e85f7f01a22b597ad66b6f6f34bad2b075
Alexey Mikhnenko <AlexeyMikhnenko@yandex.ru>
2024-11-29 14:17:24 +0300

    Supported image scaling.

--- lib/mtex/tokens/image.go
+++ lib/mtex/tokens/image.go
@@ -13,26 +13,33 @@ const (
 
 /*
 Arguments:
-name string: name of the image file
+name   string: name of the image file
+width  string: width of the image ("auto" by default)
+height string: height of the image ("auto" by default)
 
 Structure:
-<img src="{args["name"]}" class="{IMAGE_ELEMENT}">
+<img src="{args["name"]}" style="width: {args["width"]}px; height: {args["height"]}px;" class="{IMAGE_ELEMENT}">
 */
 
 type ImageToken struct {
-	name string
+	name   string
+	width  string
+	height string
 }
 
 func NewImageToken(args *map[string]string) IToken {
 	return &ImageToken{
-		name: getArgument("name", args, "undefined"),
+		name:   getArgument("name", args, "undefined"),
+		width:  getArgument("width", args, "auto"),
+		height: getArgument("height", args, "auto"),
 	}
 }
 
 func (token *ImageToken) Apply(builder *html_builder.HTMLBuilder) *html_builder.HTMLBuilder {
 	root := html_builder.NewHTMLBuilder()
 	root.AddEdge(html_builder.Wrapper{
-		Header: fmt.Sprintf("<img src=\"%s\" class=\"%s\">", token.name, IMAGE_ELEMENT),
+		Header: fmt.Sprintf("<img src=\"%s\" style=\"width: %s; height: %s;\" class=\"%s\">",
+			token.name, token.width, token.height, IMAGE_ELEMENT),
 	})
 	return root
 }
